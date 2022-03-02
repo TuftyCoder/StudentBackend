22b044f142afb9d6528a72a656374e4bff3f3700
Avnish Yadav <avnishy84@gmail.com>
2022-03-02 19:21:32 +0530

    Unknown Change

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require("./db/conn");
 const Student = require('./models/Student');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
